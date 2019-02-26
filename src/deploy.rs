dbfb68d904c0e16b2e0dd478e9a2979dec7ecc01
Frederick Hirsch <fsck@legit.solutions>
2019-02-26 09:07:18 +0100

    fix scp key and destination handling

--- src/deploy.rs
+++ src/deploy.rs
@@ -7,10 +7,9 @@ use EnokeysError;
 pub fn deploy(destinations: &[Destination]) -> Result<(), EnokeysError> {
     for destination in destinations {
         Command::new("scp")
-            .args(&["-i", "./data/id_ed25519",
-                "-P", &format!("{}", destination.port),
+            .args(&["-P", &format!("{}", destination.port),
                 "-o", "StrictHostKeyChecking=no",
-                &destination.authorized_keys_file_name.to_str().unwrap().to_string(), &format!("{}@{}:/home/{}/.ssh/authorized_keys", &destination.userauth_agent, &destination.address, &destination.userauth_agent)])
+                &destination.authorized_keys_file_name.to_str().unwrap().to_string(), &format!("{}@{}:~/.ssh/authorized_keys", &destination.userauth_agent, &destination.address)])
             .status()?;
     }
     Ok(())
