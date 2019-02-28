bce2578e2206d970d15c0393b22d0cb582e36a34
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2019-02-28 20:35:04 +0100

    Stop saving redundant information, display admin and user destinations

--- src/storage.rs
+++ src/storage.rs
@@ -2,6 +2,7 @@ extern crate dirs;
 use std::io::Write;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::path::PathBuf;
 use std::io::Read;
 use std::cmp::min;
 
@@ -10,73 +11,104 @@ use openssh_keys::PublicKey;
 use error::EnokeysError;
 use scraper;
 use USERNAME_REGEX;
-use Destination;
+use ::ADMIN_DESTINATIONS_AUTHORIZED_KEYS;
+use ::ADMIN_DESTINATIONS_STORAGE_PROVIDERS;
+use ::ADMIN_DESTINATIONS_STORAGE_RAW;
+use ::USER_DESTINATIONS_AUTHORIZED_KEYS;
+use ::USER_DESTINATIONS_STORAGE_RAW;
+use ::USER_DESTINATIONS_STORAGE_PROVIDERS;
 
-
-pub fn handle_raw_submission(name: &str, pub_key: &str, destinations: &[Destination]) -> Result<(), EnokeysError> {
-    for destination in destinations {
-        let name = USERNAME_REGEX.replace_all(name, "_");
-        let raw_storage_file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(&destination.raw_storage_file_name)?;
-        writeln!(&raw_storage_file, "{} {}@raw", &pub_key, &name)?;
-    }
+pub fn handle_raw_submission(name: &str, pub_key: &str, admin: bool) -> Result<(), EnokeysError> {
+    let raw_storage: &PathBuf = if admin {
+        &ADMIN_DESTINATIONS_STORAGE_RAW
+    } else {
+        &USER_DESTINATIONS_STORAGE_RAW
+    };
+    let name = USERNAME_REGEX.replace_all(name, "_");
+    let raw_storage_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(raw_storage)?;
+    writeln!(&raw_storage_file, "{} {}@raw", &pub_key, &name)?;
     Ok(())
 }
 
-pub fn handle_submission(provider: &str, user_name: &str, name: &str, destinations: &[Destination]) -> Result<(), EnokeysError> {
+pub fn handle_submission(provider: &str, user_name: &str, name: &str, admin: bool) -> Result<(), EnokeysError> {
     if provider.is_empty() || user_name.is_empty() {
         return Err(EnokeysError::InvalidData("username or service empty".to_string()));
     }
     let user_name = USERNAME_REGEX.replace_all(user_name, "");
     let name = USERNAME_REGEX.replace_all(name, " ");
-    for destination in destinations {
-        let mut storage_file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(&destination.providers_storage_file_name)?;
-        let line = format!("# {} \n{}:{}\n", &name, provider, &user_name);
-        println!("Adding entry:\n{}", &line);
-        write!(storage_file, "{}", &line)?;
-    }
+    let file_path: &PathBuf = if admin {
+        &ADMIN_DESTINATIONS_STORAGE_PROVIDERS
+    } else {
+        &USER_DESTINATIONS_STORAGE_PROVIDERS
+    };
+    let mut storage_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(&file_path)?;
+    let line = format!("# {} \n{}:{}\n", &name, provider, &user_name);
+    println!("Adding entry:\n{}", &line);
+    write!(storage_file, "{}", &line)?;
     Ok(())
 }
 
-pub fn generate_authorized_key_files(destinations: &[Destination]) -> Result<(), EnokeysError> {
-    for destination in destinations {
-        let mut authorized_keys_file = File::create(&destination.authorized_keys_file_name)?;
-        let mut storage_file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(&destination.providers_storage_file_name)?;
-        let mut storage_file_content = String::new();
+fn generate_authorized_key_file(authorized_keys_file_name: &PathBuf, providers_storage_file_name: &PathBuf, raw_storage_file_name: &PathBuf) -> Result<(), EnokeysError> {
+    let mut authorized_keys_file = File::create(&authorized_keys_file_name)?;
+    let mut storage_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&providers_storage_file_name)?;
+    let mut storage_file_content = String::new();
 
-        // append deploy key
-        let mut deploy_key = String::new();
-        // TODO: User-configable ssh key
-        let mut path = dirs::home_dir().unwrap();
-        path.push(".ssh");
-        path.push("id_ed25519.pub");
-        println!("Loading SSH-pubkeyfile {:?}",path);
-        if let Ok(mut deploy_key_file) = File::open(path) {
-            deploy_key_file.read_to_string(&mut deploy_key)?;
-            write!(authorized_keys_file, "{}", &deploy_key)?
+    // append deploy key
+    let mut deploy_key = String::new();
+    // TODO: User-configable ssh key
+    let mut path = dirs::home_dir().unwrap();
+    path.push(".ssh");
+    path.push("id_ed25519.pub");
+    println!("Loading SSH-pubkeyfile {:?}",path);
+    if let Ok(mut deploy_key_file) = File::open(path) {
+        deploy_key_file.read_to_string(&mut deploy_key)?;
+        write!(authorized_keys_file, "{}", &deploy_key)?
+    }
+
+    // append raw keys
+    let mut raw_keys = String::new();
+    if let Ok(mut raw_keys_file) = File::open(&raw_storage_file_name) {
+        raw_keys_file.read_to_string(&mut raw_keys)?;
+        match PublicKey::parse(&raw_keys) {
+            Ok(key) => {
+                match &key.comment {
+                    Some(ref comment) => {
+                        let comment = USERNAME_REGEX.replace_all(&comment, "_");
+                        let line = format!("{} {} {}\n", key.keytype(), base64::encode(&key.data()), &comment[0..min(comment.len(), 100)]);
+                        write!(authorized_keys_file, "{}", &line)?
+                    },
+                    None => writeln!(authorized_keys_file, "{} {}", key.keytype(), base64::encode(&key.data()))?
+                }
+            },
+            Err(e) => println!("Failed to parse PublicKey: {:?}", e)
         }
+    }
 
-        // append raw keys
-        let mut raw_keys = String::new();
-        if let Ok(mut raw_keys_file) = File::open(&destination.raw_storage_file_name) {
-            raw_keys_file.read_to_string(&mut raw_keys)?;
-            match PublicKey::parse(&raw_keys) {
+    // append keys from providers
+    storage_file.read_to_string(&mut storage_file_content)?;
+    for line in storage_file_content.split('\n').filter(|&i|!i.is_empty()).filter(|&s|&s[0..1]!="#") {
+        let entry = line.split(':').collect::<Vec<&str>>();
+        let user_keys = scraper::fetch(entry[1], entry[0])?;
+        for key in user_keys {
+            println!("parsing key: {}", &key);
+            match PublicKey::parse(&key) {
                 Ok(key) => {
                     match &key.comment {
                         Some(ref comment) => {
-                            let comment = USERNAME_REGEX.replace_all(&comment, "_");
-                            let line = format!("{} {} {}\n", key.keytype(), base64::encode(&key.data()), &comment[0..min(comment.len(), 100)]);
+                            let comment = USERNAME_REGEX.replace_all(&comment, " ");
+                            let line = format!("{} {} {}_({}@{})\n", key.keytype(), base64::encode(&key.data()), &comment[0..min(comment.len(), 100)], entry[1], entry[0]);
                             write!(authorized_keys_file, "{}", &line)?
                         },
                         None => writeln!(authorized_keys_file, "{} {}", key.keytype(), base64::encode(&key.data()))?
@@ -85,30 +117,13 @@ pub fn generate_authorized_key_files(destinations: &[Destination]) -> Result<(),
                 Err(e) => println!("Failed to parse PublicKey: {:?}", e)
             }
         }
-
-        // append keys from providers
-        storage_file.read_to_string(&mut storage_file_content)?;
-        for line in storage_file_content.split('\n').filter(|&i|!i.is_empty()).filter(|&s|&s[0..1]!="#") {
-            let entry = line.split(':').collect::<Vec<&str>>();
-            let user_keys = scraper::fetch(entry[1], entry[0])?;
-            for key in user_keys {
-                println!("parsing key: {}", &key);
-                match PublicKey::parse(&key) {
-                    Ok(key) => {
-                        match &key.comment {
-                            Some(ref comment) => {
-                                let comment = USERNAME_REGEX.replace_all(&comment, " ");
-                                let line = format!("{} {} {}_({}@{})\n", key.keytype(), base64::encode(&key.data()), &comment[0..min(comment.len(), 100)], entry[1], entry[0]);
-                                write!(authorized_keys_file, "{}", &line)?
-                            },
-                            None => writeln!(authorized_keys_file, "{} {}", key.keytype(), base64::encode(&key.data()))?
-                        }
-                    },
-                    Err(e) => println!("Failed to parse PublicKey: {:?}", e)
-                }
-            }
-        }
     }
+    Ok(())
+}
+
+pub fn generate_authorized_key_files() -> Result<(), EnokeysError> {
+    generate_authorized_key_file(&ADMIN_DESTINATIONS_AUTHORIZED_KEYS, &ADMIN_DESTINATIONS_STORAGE_PROVIDERS, &ADMIN_DESTINATIONS_STORAGE_RAW)?;
+    generate_authorized_key_file(&USER_DESTINATIONS_AUTHORIZED_KEYS, &USER_DESTINATIONS_STORAGE_PROVIDERS, &USER_DESTINATIONS_STORAGE_RAW)?;
     Ok(())
 }
 
